c66a56b2324a25a5d6d235e75bdf604511dc5164
m31 <a.mellifluous.one@gmail.com>
2020-02-05 02:38:38 -0300

    Async API done! (I think)

--- pymodule/mmapi_async.py
+++ pymodule/mmapi_async.py
@@ -1,3 +1,30 @@
 #Copyright (c) Amélia O. F. da S.
-def yep():
-    return 10
+import threading
+def _wait_move_async(device,callback):
+    move=device.wait_move()
+    callback(move)
+    return 0
+def wait_move_async(device,callback):
+    threading.Thread(target=_wait_move_async,args=(device,callback)).start()
+    return 0
+def _wait_mousedown_async(device,callback,params):
+    down=device.wait_mousedown(*params)
+    callback(down)
+    return 0
+def wait_mousedown_async(device,callback,params=(1,0,0)):
+    threading.Thread(target=_wait_mousedown_async,args=(callback,params)).start()
+    return 0
+def _wait_mouseup_async(device,callback,params):
+    up=device.wait_mouseup(*params)
+    callback(up)
+    return 0
+def wait_mouseup_async(device,callback,params=(1,0,0)):
+    threading.Thread(target=_wait_mouseup_async,args=(callback,params)).start()
+    return 0
+def _wait_scroll_async(device,callback):
+    up=device.wait_scroll()
+    callback(up)
+    return 0
+def wait_scroll_async(device,callback):
+    threading.Thread(target=_wait_mouseup_async,args=(device,callback)).start()
+    return 0
--- pymodule/tester.py
+++ pymodule/tester.py
@@ -1,5 +1,16 @@
 import mmapi
+import mmapi_async
 mouse=mmapi.Device('/dev/input/event5')
-print("Please click to continue...")
-print(str(mouse.wait_mousedown(0,0,1)))
-print("\nThanks!")
+print("Please right click to continue...")
+mouse.wait_mousedown(0,0,1)
+print("Thanks!")
+print("Now left click...")
+mouse.wait_mousedown()
+print("Thanks!")
+print("Now left click again...")
+def move(dir):
+    print("Mouse moved during wait!")
+mmapi_async.wait_move_async(mouse,move)
+mouse.wait_mousedown()
+print("Once more, please.")
+mouse.wait_mousedown()
